309cb663615b169740648140d7bc304c41c70f12
Kazi Namira Meyheg Sanam <namirameyheg@gmail.com>
2025-08-15 03:50:44 +0600

    Upload files

--- V67- DOM~ Children, Parents, Sibiling Nodes  in JavaScripts/script.js
+++ V67- DOM~ Children, Parents, Sibiling Nodes  in JavaScripts/script.js
@@ -1,1 +1,9 @@
 console.log("Hellooooo")
+
+document.body.firstElementChild
+document.body.firstElementChild.childNodes
+document.body.firstElementChild.children
+
+//  firstElementChild → First HTML element inside body.
+//  childNodes → All things inside it (including text and spaces).
+//  children → Only HTML elements inside it.
---
+++ V68- Selecting by Ids, Classes, and More in JavaScript/script.js
@@ -0,0 +1,24 @@
+console.log("Hellllloooo");
+let boxes = document.getElementsByClassName("box");
+console.log(boxes);
+
+boxes[2].style.backgroundColor = "red";
+
+// document.getElementById("redbox").style.backgroundColor = "red";
+
+document.querySelector(".box").style.backgroundColor = "green";
+console.log(document.querySelectorAll(".box"));
+
+document.querySelectorAll(".box").forEach((e) => {
+  e.style.backgroundColor = "green";
+});
+
+//  getElementById() → when you need to select a single element with a unique ID.
+
+//  getElementsByClassName() → to select all elements with a specific class.
+
+//  getElementsByTagName() → to select all elements of a particular tag.
+
+//  querySelector() → for selecting the first element that matches a CSS selector.
+
+//  querySelectorAll() → to select all elements that match a CSS selector.
